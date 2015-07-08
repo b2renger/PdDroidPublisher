68d1a19cd7ee07f0b625bff91640802b564ddeb1
mgsx-dev <germain.aubert@gmail.com>
2015-07-08 12:07:41 +0200

    bug fix error when setting BPM without start clock.

--- pd-party/src/cx/mccormick/pddroidparty/midi/MidiClock.java
+++ pd-party/src/cx/mccormick/pddroidparty/midi/MidiClock.java
@@ -103,8 +103,11 @@ public class MidiClock
 	{
 		bpm = value;
 		
-		long period = 60000000000L / (long)(bpm * 24);
-		timer.setPeriod(period, TimeUnit.NANOSECONDS);
+		if(timer != null)
+		{
+			long period = 60000000000L / (long)(bpm * 24);
+			timer.setPeriod(period, TimeUnit.NANOSECONDS);
+		}
 	}
 
 	public void resume(final NetworkMidiOutput out, final int startBpm) 
