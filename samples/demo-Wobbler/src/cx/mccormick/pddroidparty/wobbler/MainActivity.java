b060287e46058f7bd74c553238e9234aea94a18e
b2renger <berenger.recoules@gmail.com>
2015-09-22 21:22:16 +0200

    big fat commit to straighten things up
    wobbler is broken - something wrong with the patch
    (sorry @mgsx - it will be better in the future)

---
+++ samples/demo-Wobbler/src/cx/mccormick/pddroidparty/wobbler/MainActivity.java
@@ -0,0 +1,24 @@
+package cx.mccormick.pddroidparty.wobbler;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cx.mccormick.pddroidparty.PdDroidPartyConfig;
+import cx.mccormick.pddroidparty.PdDroidPartyLauncher;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        PdDroidPartyConfig config = new PdDroidPartyConfig();
+        config.midiClockMaxBPM = 480;
+        
+        PdDroidPartyLauncher.launch(this, "Wobbler/wobbler.pd", config);
+    }
+    
+
+
+
+}
