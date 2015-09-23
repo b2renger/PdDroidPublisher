34826ecf7812a5dbe26263aa347d51b5b319d569
b2renger <berenger.recoules@gmail.com>
2015-09-23 17:51:22 +0200

    adding demo-ChipSequencer project

---
+++ samples/demo-ChipSequencer/src/cx/mccormick/pddroidparty/chipsequencer/MainActivity.java
@@ -0,0 +1,24 @@
+package cx.mccormick.pddroidparty.chipsequencer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cx.mccormick.pddroidparty.PdDroidPartyConfig;
+import cx.mccormick.pddroidparty.PdDroidPartyLauncher;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        PdDroidPartyConfig config = new PdDroidPartyConfig();
+        config.midiClockMaxBPM = 480;
+        
+        PdDroidPartyLauncher.launch(this, "Chip_sequencer/chip_sequencer.pd", config);
+    }
+    
+
+
+
+}
