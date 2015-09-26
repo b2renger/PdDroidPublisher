171b86c9560bece0f4418cb4003a9566ae3bde8e
mgsx-dev <germain.aubert@gmail.com>
2015-09-26 15:12:56 +0200

    theme wobbler customization

--- samples/demo-Wobbler/src/cx/mccormick/pddroidparty/wobbler/MainActivity.java
+++ samples/demo-Wobbler/src/cx/mccormick/pddroidparty/wobbler/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import cx.mccormick.pddroidparty.PdDroidPartyConfig;
 import cx.mccormick.pddroidparty.PdDroidPartyLauncher;
+import cx.mccormick.pddroidparty.theme.mono.MonochromeTheme;
 
 
 public class MainActivity extends Activity {
@@ -14,6 +15,7 @@ public class MainActivity extends Activity {
         
         PdDroidPartyConfig config = new PdDroidPartyConfig();
         config.midiClockMaxBPM = 480;
+        config.theme = new MonochromeTheme(MonochromeTheme.BLUE, true);
         
         PdDroidPartyLauncher.launch(this, "Wobbler/wobbler.pd", config);
     }
