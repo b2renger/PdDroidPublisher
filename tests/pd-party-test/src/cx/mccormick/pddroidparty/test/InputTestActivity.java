d802f6094aa80d6cd385cc63132836e78d8526cd
mgsx-dev <germain.aubert@gmail.com>
2015-09-29 17:02:44 +0200

    add microphone test project

---
+++ tests/pd-party-test/src/cx/mccormick/pddroidparty/test/InputTestActivity.java
@@ -0,0 +1,26 @@
+package cx.mccormick.pddroidparty.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cx.mccormick.pddroidparty.PdDroidPartyConfig;
+import cx.mccormick.pddroidparty.PdDroidPartyLauncher;
+import cx.mccormick.pddroidparty.theme.mono.MonochromeTheme;
+
+
+public class InputTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        PdDroidPartyConfig config = new PdDroidPartyConfig();
+        config.theme = new MonochromeTheme(0xff101933, 0xff202943, 0xff004ce6);
+        config.guiKeepAspectRatio = true;
+
+        PdDroidPartyLauncher.launch(this, "inputTest/main.pd", config);
+    }
+    
+
+
+
+}
