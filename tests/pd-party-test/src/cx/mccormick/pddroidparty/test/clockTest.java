8d1122f746490c3ca4b44cc751868b38ea98a1d4
b2renger <berenger.recoules@gmail.com>
2015-10-01 19:53:54 +0200

    add clock test

---
+++ tests/pd-party-test/src/cx/mccormick/pddroidparty/test/clockTest.java
@@ -0,0 +1,26 @@
+package cx.mccormick.pddroidparty.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cx.mccormick.pddroidparty.PdDroidPartyConfig;
+import cx.mccormick.pddroidparty.PdDroidPartyLauncher;
+import cx.mccormick.pddroidparty.theme.mono.MonochromeTheme;
+
+
+public class clockTest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        PdDroidPartyConfig config = new PdDroidPartyConfig();
+        config.theme = new MonochromeTheme(0xff101933, 0xff202943, 0xff004ce6);
+        config.guiKeepAspectRatio = true;
+
+        PdDroidPartyLauncher.launch(this, "clockTest/clockTest.pd", config);
+    }
+    
+
+
+
+}
