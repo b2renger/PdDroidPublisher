4f40556b1fcafe411ec31f779d97d90f6ba75900
b2renger <berenger.recoules@gmail.com>
2015-10-04 19:04:18 +0200

    major refactor of AcidBass demo
    uses custom taplist implementation

--- samples/demo-AcidBass/src/cx/mccormick/pddroidparty/acidbass/MainActivity.java
+++ samples/demo-AcidBass/src/cx/mccormick/pddroidparty/acidbass/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import cx.mccormick.pddroidparty.PdDroidPartyConfig;
 import cx.mccormick.pddroidparty.PdDroidPartyLauncher;
+import cx.mccormick.pddroidparty.theme.mono.MonochromeTheme;
+import cx.mccormick.pddroidparty.widget.abs.Taplist;
 
 
 public class MainActivity extends Activity {
@@ -13,7 +15,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         
         PdDroidPartyConfig config = new PdDroidPartyConfig();
+       
         config.midiClockMaxBPM = 480;
+        config.guiKeepAspectRatio = true;
+        config.theme = new MonochromeTheme(MonochromeTheme.RED, true);
+        config.typeOverrides.put(Taplist.class, CustomTaplist.class);
         
         PdDroidPartyLauncher.launch(this, "Acid_Bass/AcidBass.pd", config);
     }
