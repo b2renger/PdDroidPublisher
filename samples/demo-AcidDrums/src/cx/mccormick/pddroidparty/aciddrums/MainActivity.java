e591d062943afe7b3b643e4a13bc5072dadd4dc3
b2renger <berenger.recoules@gmail.com>
2015-10-07 22:58:46 +0200

    update aciddrums add theme

--- samples/demo-AcidDrums/src/cx/mccormick/pddroidparty/aciddrums/MainActivity.java
+++ samples/demo-AcidDrums/src/cx/mccormick/pddroidparty/aciddrums/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import cx.mccormick.pddroidparty.PdDroidPartyConfig;
 import cx.mccormick.pddroidparty.PdDroidPartyLauncher;
+import cx.mccormick.pddroidparty.theme.mono.MonochromeTheme;
 
 
 public class MainActivity extends Activity {
@@ -14,6 +15,8 @@ public class MainActivity extends Activity {
         
         PdDroidPartyConfig config = new PdDroidPartyConfig();
         config.midiClockMaxBPM = 480;
+        config.guiKeepAspectRatio = true;
+        config.theme = new MonochromeTheme(MonochromeTheme.YELLOW,true);
         
         PdDroidPartyLauncher.launch(this, "Acid_Drums/AcidDrums.pd", config);
     }
