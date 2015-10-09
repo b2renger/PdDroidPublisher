869fda1a1a82e0db4d225b912432ede467e7e275
mgsx-dev <germain.aubert@gmail.com>
2015-10-09 12:53:45 +0200

    android persist test

---
+++ tests/pd-party-test/src/cx/mccormick/pddroidparty/test/PersistTestActivity.java
@@ -0,0 +1,24 @@
+package cx.mccormick.pddroidparty.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cx.mccormick.pddroidparty.PdDroidPartyConfig;
+import cx.mccormick.pddroidparty.PdDroidPartyLauncher;
+
+
+public class PersistTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        PdDroidPartyConfig config = new PdDroidPartyConfig();
+        config.guiKeepAspectRatio = true;
+
+        PdDroidPartyLauncher.launch(this, "PersistTest/PersistTestAndroid.pd", config);
+    }
+    
+
+
+
+}
