c75f5cdfa36e460c953ae3e20c2bda3b871c9585
b2renger <berenger.recoules@gmail.com>
2015-10-09 19:55:16 +0200

    Splitting AcidDrums into abstractions :
    - main is for the main sequencer
    - controls is for the synths parameters
    - workings is the audio core, it called in main
    - AcidDrums.pd now references workings as audio core this way it's
    possible to have to possible gui for the same patch and be able to
    switch beetween them in the main activity.

--- samples/demo-AcidDrums/src/cx/mccormick/pddroidparty/aciddrums/MainActivity.java
+++ samples/demo-AcidDrums/src/cx/mccormick/pddroidparty/aciddrums/MainActivity.java
@@ -18,7 +18,13 @@ public class MainActivity extends Activity {
         config.guiKeepAspectRatio = true;
         config.theme = new MonochromeTheme(MonochromeTheme.YELLOW,true);
         
-        PdDroidPartyLauncher.launch(this, "Acid_Drums/AcidDrums.pd", config);
+       
+        
+        config.patches.put("Sequencer", "Acid_Drums/main.pd");
+        config.patches.put("Controls", "Acid_Drums/controls.pd");
+        
+        PdDroidPartyLauncher.launch(this, config);
+        //PdDroidPartyLauncher.launch(this,"Acid_Drums/AcidDrums.pd", config);
     }
     
 
