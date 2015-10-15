7fbee29083f0554043bbfb3fca32549c3db2c202
mgsx-dev <germain.aubert@gmail.com>
2015-10-15 15:46:04 +0200

    use popup taplist in wobbler sample fix #3

--- samples/demo-Wobbler/src/cx/mccormick/pddroidparty/wobbler/MainActivity.java
+++ samples/demo-Wobbler/src/cx/mccormick/pddroidparty/wobbler/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import cx.mccormick.pddroidparty.PdDroidPartyConfig;
 import cx.mccormick.pddroidparty.PdDroidPartyLauncher;
 import cx.mccormick.pddroidparty.theme.mono.MonochromeTheme;
+import cx.mccormick.pddroidparty.widget.custom.PopupTaplist;
 
 
 public class MainActivity extends Activity {
@@ -18,6 +19,7 @@ public class MainActivity extends Activity {
         config.objectOverrides.put("TrackLeds", TrackLed.class);
         config.objectOverrides.put("WobbleBinary", WobbleSelector.class);
         config.objectOverrides.put("WobbleTernary", WobbleSelector.class);
+        config.objectOverrides.put("TuneTaplist", PopupTaplist.class);
         config.theme = new MonochromeTheme(0xff101933, 0xff202943, 0xff004ce6);
         
         config.presetsPaths.add("Wobbler/savefiles");
