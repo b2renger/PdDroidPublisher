f47a0a6b833ae9b1ae5b27d9889ec28838255494
b2renger <berenger.recoules@gmail.com>
2015-10-21 20:44:47 +0200

    #40 update chorder

--- samples/demo-Chorder/src/net/mgsx/ppp/samples/chorder/MainActivity.java
+++ samples/demo-Chorder/src/net/mgsx/ppp/samples/chorder/MainActivity.java
@@ -6,7 +6,9 @@ import net.mgsx.ppp.PdDroidPartyConfig;
 import net.mgsx.ppp.PdDroidPartyLauncher;
 import net.mgsx.ppp.theme.mono.MonochromeTheme;
 import net.mgsx.ppp.widget.abs.Taplist;
+import net.mgsx.ppp.widget.core.Slider;
 import net.mgsx.ppp.widget.custom.PopupTaplist;
+import net.mgsx.ppp.widget.custom.RibbonSlider;
 
 
 public class MainActivity extends Activity {
@@ -16,11 +18,24 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         
         PdDroidPartyConfig config = new PdDroidPartyConfig();
+        config.theme = new MonochromeTheme(MonochromeTheme.RED, true);
         
-        PdDroidPartyLauncher.launch(this, "Chorder/Chorder.pd", config);
+        config.guiKeepAspectRatio = true;
+        
+        config.typeOverrides.put(Taplist.class, PopupTaplist.class);
+        config.typeOverrides.put(Slider.class, RibbonSlider.class);
+        
+        config.presetsPaths.add("Chorder/presets_chords");
+        config.presetsPaths.add("Chorder/presets_synth");
+        
+        config.guiPatches.put("Play Board", "Chorder/Chorder-playBoard.pd");
+        config.guiPatches.put("Chords selection", "Chorder/Chorder-gui.pd");
+        config.guiPatches.put("Audio Controls", "Chorder/Chorder-controls.pd");
+        config.corePatches.add("Chorder/Chorder-core.pd");
+        
+        PdDroidPartyLauncher.launch(this, config);
     }
+        
     
-
-
 
 }
