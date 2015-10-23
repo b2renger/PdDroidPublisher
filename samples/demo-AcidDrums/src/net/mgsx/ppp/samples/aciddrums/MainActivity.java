150b168224ec520a95c8c53db1d8cc3f75134233
b2renger <berenger.recoules@gmail.com>
2015-10-23 14:47:06 +0200

    #40 aciddrums : fix loading, add custom widget for bangs, sliders

--- samples/demo-AcidDrums/src/net/mgsx/ppp/samples/aciddrums/MainActivity.java
+++ samples/demo-AcidDrums/src/net/mgsx/ppp/samples/aciddrums/MainActivity.java
@@ -3,6 +3,10 @@ package net.mgsx.ppp.samples.aciddrums;
 import net.mgsx.ppp.PdDroidPartyConfig;
 import net.mgsx.ppp.PdDroidPartyLauncher;
 import net.mgsx.ppp.theme.mono.MonochromeTheme;
+import net.mgsx.ppp.widget.core.Bang;
+import net.mgsx.ppp.widget.core.Slider;
+import net.mgsx.ppp.widget.custom.RibbonSlider;
+import net.mgsx.ppp.widget.custom.SimpleBang;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -16,10 +20,15 @@ public class MainActivity extends Activity {
         PdDroidPartyConfig config = new PdDroidPartyConfig();
         config.guiKeepAspectRatio = true;
         config.theme = new MonochromeTheme(MonochromeTheme.YELLOW,true);
+        
+       
+        config.typeOverrides.put(Slider.class, RibbonSlider.class);
+        config.typeOverrides.put(Bang.class, SimpleBang.class);
        
        // config.patches.put("Full Stack", "Acid_Drums/aciddrums_fullstack.pd");
         config.guiPatches.put("Sequencer", "Acid_Drums/aciddrums_sequencer.pd");
         config.guiPatches.put("Audio Controls", "Acid_Drums/aciddrums_controls.pd");
+        config.corePatches.add("Acid_Drums/aciddrums_audiocore.pd");
         
         config.presetsPaths.add("Acid_Drums/presets_pattern");
         config.presetsPaths.add("Acid_Drums/presets_synth");
