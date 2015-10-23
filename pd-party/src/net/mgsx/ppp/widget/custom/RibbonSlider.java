3822bab1dbeb02c7b85c57e2e718d7afddbcf53c
b2renger <berenger.recoules@gmail.com>
2015-10-23 18:38:36 +0200

    #41 RibbonSlider is now rounded

--- pd-party/src/net/mgsx/ppp/widget/custom/RibbonSlider.java
+++ pd-party/src/net/mgsx/ppp/widget/custom/RibbonSlider.java
@@ -4,6 +4,7 @@ import net.mgsx.ppp.view.PdDroidPatchView;
 import net.mgsx.ppp.widget.core.Slider;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 
 /**
  * Display slider with filled rectangle (from minimum to current value) instead of a cursor. 
@@ -18,7 +19,7 @@ public class RibbonSlider extends Slider
 	{
 		paint.setColor(bgcolor);
 		paint.setStyle(Paint.Style.FILL);
-		canvas.drawRect(dRect, paint);
+		canvas.drawRoundRect(dRect,5,5, paint);
 
 		paint.setColor(fgcolor);
 		paint.setStyle(Paint.Style.FILL);
@@ -26,18 +27,21 @@ public class RibbonSlider extends Slider
 		if(horizontal)
 		{
 			float offset = dRect.width() * (val - min) / (max - min);
-			canvas.drawRect(dRect.left, dRect.top, dRect.left + offset, dRect.bottom, paint);
+			RectF rect = new RectF(dRect.left, dRect.top, dRect.left + offset, dRect.bottom);
+			//canvas.drawRect);
+			canvas.drawRoundRect(rect,5,5,paint);
 		}
 		else
 		{
 			float offset = dRect.height() * (val - min) / (max - min);
-			canvas.drawRect(dRect.left, dRect.bottom - offset, dRect.right, dRect.bottom, paint);
+			RectF rect = new RectF(dRect.left, dRect.bottom - offset, dRect.right, dRect.bottom);
+			canvas.drawRoundRect(rect,5,5,paint);
 		}
 
 		paint.setColor(fgcolor);
 		paint.setStyle(Paint.Style.STROKE);
 		paint.setStrokeWidth(1);
-		canvas.drawRect(dRect, paint);
+		canvas.drawRoundRect(dRect,5,5, paint);
 		paint.setStyle(Paint.Style.FILL);
 		drawLabel(canvas);
 	}
