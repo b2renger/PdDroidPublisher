064dd8cc158b305a532314c9e915c417e426eb04
b2renger <berenger.recoules@gmail.com>
2015-12-13 23:33:25 +0100

    updating Chip-sequencer

--- samples/demo-ChipSequencer/src/net/mgsx/ppp/samples/chipsequencer/MainActivity.java
+++ samples/demo-ChipSequencer/src/net/mgsx/ppp/samples/chipsequencer/MainActivity.java
@@ -3,6 +3,16 @@ package net.mgsx.ppp.samples.chipsequencer;
 import net.mgsx.ppp.PdDroidPartyConfig;
 import net.mgsx.ppp.PdDroidPartyLauncher;
 import net.mgsx.ppp.theme.mono.MonochromeTheme;
+import net.mgsx.ppp.widget.abs.Taplist;
+import net.mgsx.ppp.widget.core.Bang;
+import net.mgsx.ppp.widget.core.Radio;
+import net.mgsx.ppp.widget.core.Slider;
+import net.mgsx.ppp.widget.core.Toggle;
+import net.mgsx.ppp.widget.custom.FilledToggle;
+import net.mgsx.ppp.widget.custom.PopupTaplist;
+import net.mgsx.ppp.widget.custom.RibbonSlider;
+import net.mgsx.ppp.widget.custom.SimpleBang;
+import net.mgsx.ppp.widget.custom.SimpleRadio;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -14,8 +24,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         
         PdDroidPartyConfig config = new PdDroidPartyConfig();
-        config.guiKeepAspectRatio = false;
-        config.theme = new MonochromeTheme(MonochromeTheme.ORANGE, true);
+        config.guiKeepAspectRatio = true;
+        
+        config.theme = new MonochromeTheme(MonochromeTheme.YELLOW, true);
+        
+        config.typeOverrides.put(Taplist.class, PopupTaplist.class);
+        config.typeOverrides.put(Slider.class, RibbonSlider.class);
+        config.typeOverrides.put(Bang.class, SimpleBang.class);
+        config.typeOverrides.put(Toggle.class, FilledToggle.class);
+        config.typeOverrides.put(Radio.class, SimpleRadio.class);
        
         config.presetsPaths.add("Chip_sequencer/presets_pattern");
         config.presetsPaths.add("Chip_sequencer/presets_synth");
@@ -23,6 +40,7 @@ public class MainActivity extends Activity {
        
         config.guiPatches.put("Sequencer", "Chip_sequencer/chip_sequencer_sequencer.pd");
         config.guiPatches.put("Audio Controls", "Chip_sequencer/chip_sequencer_controls.pd");
+        config.corePatches.add("Chip_sequencer/chip_sequencer_audiocore.pd");
         
         PdDroidPartyLauncher.launch(this, config);
     }
